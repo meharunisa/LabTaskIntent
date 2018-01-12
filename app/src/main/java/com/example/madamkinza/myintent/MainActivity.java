f9812fa906cb7db6d9f631bb6cd7c9414ed71d5f
meharunisa2233 <meharunisa2233@gmail.com>
2018-01-12 10:32:51 -0800

    Initial commit

---
+++ app/src/main/java/com/example/madamkinza/myintent/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.madamkinza.myintent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.Menu;
+import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        Intent intent = new Intent("implicitaccess");
+        startActivity(intent);
+    }
+    }
+
